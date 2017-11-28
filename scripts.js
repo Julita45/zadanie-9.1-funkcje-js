e92fecf1eff8b45fc3ec87244eda712f80867ab8
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-11-28 19:47:51 +0100

    Add scripts gitignore and json

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function getTriangleArea(a, h) {
+	if (a && h > 0) {
+		return a * h / 2;
+	} else {
+		return 'Incorrect data';
+	}
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(0, 15);
+console.log(getTriangleArea(0, 15));
+
+var triangle2Area = getTriangleArea(5, 4);
+console.log(getTriangleArea(5, 4));
+
+var triangle3Area = getTriangleArea(7, 20);
+console.log(getTriangleArea(7, 20));
