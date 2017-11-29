9bfad8f08148fb302e9dce7fb66abcd1d86f8226
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-11-29 18:38:24 +0100

    Change scripts

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 function getTriangleArea(a, h) {
 	if (a && h > 0) {
 		return a * h / 2;
-	} else {
-		return 'Incorrect data';
-	}
-} 
+	} 
+	return 'Incorrect data';
+}
+
+console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(0, 15);
-console.log(getTriangleArea(0, 15));
+console.log(triangle1Area);
 
 var triangle2Area = getTriangleArea(5, 4);
 console.log(getTriangleArea(5, 4));
